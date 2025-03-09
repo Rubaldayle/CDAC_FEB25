3d31a724de013222bc30faad9296d2235d1c8009
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 07:00:00 +0530

    Update Question6.java

--- Assignment2/Question6.java
+++ Assignment2/Question6.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Question6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+      
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+      
+        System.out.print("Enter third number: ");
+        int c = scanner.nextInt();
+        scanner.close();
+      
+        int largest = (a > b) 
+                      ? ((a > c) ? a : c) 
+                      : ((b > c) ? b : c);
+      
+        System.out.println("The largest number is: " + largest);
+    }
+}
