e7b16415030c7c5cdf3149c7d334698c24c4b8b3
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 07:12:15 +0530

    Question8.java

--- Assignment2/Question8.java
+++ Assignment2/Question8.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class Question8  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+      
+        System.out.print("Enter first boolean value: ");
+        boolean a = scanner.nextBoolean();
+      
+        System.out.print("Enter second boolean value: ");
+        boolean b = scanner.nextBoolean();
+      
+        System.out.print("Enter third boolean value: ");
+        boolean c = scanner.nextBoolean();
+        scanner.close();
+
+        boolean result = (a && b) || (a && c) || (b && c);
+
+        System.out.println("At least two values are true: " + result);
+    }
+}
 
