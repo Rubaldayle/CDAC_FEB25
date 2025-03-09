1a8a1bcb36c8080967242632c071eabb971d52cc
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 08:11:30 +0530

    Question19.java

--- Assignment2/Question19.java
+++ Assignment2/Question19.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+
+public class Question19 
+{
+    public static void main(String[] args)
+  {
+        Scanner scanner = new Scanner(System.in);
+      
+        System.out.print("Enter a character: ");
+        char ch = scanner.next().charAt(0);
+        scanner.close();
+
+        String result = (ch >= 'A' && ch <= 'Z') ? "Uppercase Letter"
+                     : (ch >= 'a' && ch <= 'z') ? "Lowercase Letter"
+                     : "Not a Letter";
+
+        System.out.println("The character '" + ch + "' is: " + result);
+    }
+}
 
