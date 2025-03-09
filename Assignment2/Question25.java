526d4307c9c6fc38451d09e4ab1c2ee2d91e45b9
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 11:30:52 +0530

    Question25.java

--- Assignment2/Question25.java
+++ Assignment2/Question25.java
@@ -1,1 +1,24 @@
 
+import java.util.Scanner;
+
+public class Question25
+ {
+    public static void main(String[] args) 
+	{
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        int rev = 0;
+    
+        while (num != 0) 
+        {
+            rev = rev * 10 + num % 10; 
+            num /= 10; 
+        }
+        System.out.println("Reversed number: " + rev);
+    }
+}
+
