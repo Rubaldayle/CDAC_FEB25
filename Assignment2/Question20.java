ab4358972c840bd4bb11f7bf1b2a453b31c74a66
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 08:34:27 +0530

    Question20.java

--- Assignment2/Question20.java
+++ Assignment2/Question20.java
@@ -1,1 +1,18 @@
 
+import java.util.Scanner;
+
+public class Question20 
+{
+    public static void main(String[] args) 
+  {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        int absValue = (num < 0) ? -num : num;
+
+        System.out.println("Absolute value: " + absValue);
+    }
+}
