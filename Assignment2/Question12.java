3bcf065684bccbe34a613cf5754b442a817adafc
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 07:32:38 +0530

    Question12.java

--- Assignment2/Question12.java
+++ Assignment2/Question12.java
@@ -1,1 +1,19 @@
+//Write a Java program to multiply a number by 8 without using * or / operators.
+//Hint: Use bitwise left shift (<<).
+
+import java.util.Scanner;
+
+public class Question12 {
+    public static void main(String[] args) 
+	{
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+    
+        int result = num << 3; 
+        System.out.println("Result after multiplying by 8: " + result);
+    }
+}
 
