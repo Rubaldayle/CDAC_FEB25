7c398aa34fb5b19ca9b5c327d0cb4b3d91ba087c
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 07:39:17 +0530

    Question13.java

--- Assignment2/Question13.java
+++ Assignment2/Question13.java
@@ -1,1 +1,21 @@
+ // Implement a Java program to find the absolute value of an integer using bitwise
+//operators.
+//Hint: mask = num >> 31; abs = (num + mask) ^ mask;
+
+ import java.util.Scanner;
+
+public class Question13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+        scanner.close();
+      
+        int mask = num >> 31; 
+        int absValue = (num + mask) ^ mask; 
+
+        System.out.println("Absolute value: " + absValue);
+    }
+}
 
