772fff2c6bedbc6350ac65111490829c9febbd7d
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 10:36:29 +0530

    Question23.java

--- Assignment2/Question23.java
+++ Assignment2/Question23.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+
+public class Question23
+  {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+      
+        String result = (num & 1) == 0 ? "Even" : "Odd";
+
+        System.out.println("The number is: " + result);
+    }
+}
 
