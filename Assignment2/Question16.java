5a9e595578394567eafba4395b16116afd8eafb7
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 07:50:44 +0530

    Question16.java

--- Assignment2/Question16.java
+++ Assignment2/Question16.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+
+public class Question16 
+{
+    public static void main(String[] args)
+  {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+     String result = (num > 0) ? "Positive"
+                     : (num < 0) ? "Negative"
+                     : "Zero";
+
+        System.out.println("The number is: " + result);
+    }
+}
 
