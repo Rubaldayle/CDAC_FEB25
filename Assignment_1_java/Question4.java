cd21eede03bd68a221bb6de8ea302cb440f17e0c
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 12:29:45 +0530

    Update Question4.java

--- Assignment_1_java/Question4.java
+++ Assignment_1_java/Question4.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+
+public class Question4
+  {
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int intValue = scanner.nextInt();
+
+        Double doubleValue = Double.valueOf(intValue);
+
+                            System.out.println("Converted to Double: " + doubleValue);
+
+        scanner.close();
+    }
+}
 
