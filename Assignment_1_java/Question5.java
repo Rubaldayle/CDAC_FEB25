d99a93acad850bb82e6ffe38e4465d43f7404932
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 12:39:23 +0530

    Question5.java

--- Assignment_1_java/Question5.java
+++ Assignment_1_java/Question5.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
+
+public class Question5
+  {
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number a: ");
+        int a = scanner.nextInt();
+      
+        System.out.print("Enter second number b: ");
+        int b = scanner.nextInt();
+        scanner.close();
+
+                        System.out.println("\nBefore Swapping:");
+                        System.out.println("a = " + a + ", b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+                         System.out.println("\nAfter Swapping:");
+                         System.out.println("a = " + a + ", b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("\nAfter Swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
 
