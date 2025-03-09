a259b028d7cc836936589a448390bee25b1d1111
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 10:20:55 +0530

    Question22.java

--- Assignment2/Question22.java
+++ Assignment2/Question22.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
 
+public class Question22 
+{
+    public static void main(String[] args)
+  {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter an operator (+, -, *): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        scanner.close();
+
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+                default:
+                System.out.println("Invalid operator! Please use +, -, *.");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
