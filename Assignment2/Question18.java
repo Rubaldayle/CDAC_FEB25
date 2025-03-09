ea1ef9fb64329533dfd9c9fa764b34da8c12811a
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 08:05:25 +0530

    Question18.java

--- Assignment2/Question18.java
+++ Assignment2/Question18.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class Question18 
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter student's percentage: ");
+        double percentage = scanner.nextDouble();
+        scanner.close();
+
+        String result = (percentage >= 40) ? "Pass" : "Fail";
+        System.out.println("Result: " + result);
+    }
+}
