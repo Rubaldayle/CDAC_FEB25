127cb7154fbb1900bbf19f6133b1a19c4773cad6
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 07:19:25 +0530

    Question9.java

--- Assignment2/Question9.java
+++ Assignment2/Question9.java
@@ -3,6 +3,8 @@
 //50) without using if-else.
 //Hint: Use logical AND (&&) in a print statement.import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class Question9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
