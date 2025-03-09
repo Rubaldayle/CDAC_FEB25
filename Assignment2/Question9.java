e130aa2a7b4679a154fae16d38184d50723ec8d3
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 07:16:09 +0530

    Question9.java

--- Assignment2/Question9.java
+++ Assignment2/Question9.java
@@ -1,1 +1,18 @@
 
+ //Implement a Java program that checks if a number is within a specific range (20 to
+//50) without using if-else.
+//Hint: Use logical AND (&&) in a print statement.import java.util.Scanner;
+
+public class Question9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Number is in range (20 to 50): " + (num >= 20 && num <= 50));
+    }
+}
+
