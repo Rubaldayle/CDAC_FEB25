8619e2cb21b2832b318b13d0477d91995041337f
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 07:28:20 +0530

    Question11.java

--- Assignment2/Question11.java
+++ Assignment2/Question11.java
@@ -1,1 +1,19 @@
 
+//Write a program to check if a given number is a power of 2 using bitwise operators.
+//Hint: n & (n - 1) == 0 for positive numbers.
+
+public class Question11{
+	public static void main(String[] args)
+	{
+		check(8);
+		check(7);
+	}
+	
+	public static void check(int a){
+		if(a<0) a = -a;
+		if((a & (a-1))==0)
+			System.out.println(a + " is a power of two");
+		else
+			System.out.println(a + " is not a power of two");	
+	} 
+}	
