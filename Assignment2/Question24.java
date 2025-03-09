5fbee221d6618e5aa5c5b2736c7cacf56f228bdb
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 11:12:47 +0530

    Question24.java

--- Assignment2/Question24.java
+++ Assignment2/Question24.java
@@ -1,1 +1,16 @@
+public class Question24 
+{
+    public static void main(String[] args) 
+  {
+        System.out.println("Even numbers from 1 to 100:");
+
+        for (int i = 1; i <= 100; i++)
+          {
+            if ((i & 1) == 0) 
+            {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
 
