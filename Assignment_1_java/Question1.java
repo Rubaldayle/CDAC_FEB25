3a7f6e3da3219fc7ca5dd3837deb196a5bb4517a
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 12:01:06 +0530

    Update Question1.java

--- Assignment_1_java/Question1.java
+++ Assignment_1_java/Question1.java
@@ -1,1 +1,27 @@
+//Write a Java program to declare and initialize all eight primitive data types and print
+//their values
+
+public class Question1 
+{
+    public static void main(String[] args) 
+  {
+        byte byteVar = 127;         
+        short shortVar = 32000;     
+        int intVar = 100000;        
+        long longVar = 1000000000L; 
+        float floatVar = 10.5f;     
+        double doubleVar = 20.12345;
+        char charVar = 'A';        
+        boolean booleanVar = true;  
+
+        System.out.println("byte value: " + byteVar);
+        System.out.println("short value: " + shortVar);
+        System.out.println("int value: " + intVar);
+        System.out.println("long value: " + longVar);
+        System.out.println("float value: " + floatVar);
+        System.out.println("double value: " + doubleVar);
+        System.out.println("char value: " + charVar);
+        System.out.println("boolean value: " + booleanVar);
+    }
+}
 
