c10f28dd67bf81d01a7779b65c754bf6695442b4
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-09 12:18:06 +0530

    Update Question3.java

--- Assignment_1_java/Question3.java
+++ Assignment_1_java/Question3.java
@@ -1,1 +1,28 @@
+public class Question3 
+{
+    public static void main(String[] args)
+  {
+        // Implicit Type Casting (Widening) 
+        int intVal = 100;
+        double doubleVal = intVal; 
+
+                  System.out.println("Integer value: " + intVal);
+                  System.out.println("Converted to Double: " + doubleVal);
+
+        // Explicit Type Casting (Narrowing)
+        double originalDouble = 99.99;
+        int castedInt = (int) originalDouble; 
+        
+                   System.out.println("Double value: " + originalDouble);
+                    System.out.println("Converted to Integer: " + castedInt);
+
+        
+        long longVal = 123456789L;
+        short shortVal = (short) longVal; 
+
+         
+                      System.out.println("Long value: " + longVal);
+                      System.out.println("Converted to Short: " + shortVal);
+    }
+}
 
