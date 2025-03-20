27e54379226a3f7471822c7f43f2239a2e8b2cdd
Rubaldayle <167878553+Rubaldayle@users.noreply.github.com>
2025-03-20 22:05:34 +0530

    Update Question5.java

--- Java Assignment 3/Question5.java
+++ Java Assignment 3/Question5.java
@@ -1,1 +1,47 @@
+import java.util.Scanner;
+
+class BMI {
+double height;
+double weight;
+
+BMI(double height, double weight)
+{
+this.height = height;
+this.weight = weight;
+     }
+ public double getheight(){
+ return height;
+ }
+public double getweight(){
+ return weight;
+ }
+ public void setheight(double height){
+	 this.height = height;
+      }
+	  
+ public void setweight(double weight){
+	 this.weight = weight;
+    }
+	
+ public double calculateBMI(){
+ return weight/(height*height);
+ }
+
+ }
+class Question5{
+public static void main(String[] args){
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.println("Enter your height:");
+	double height = scanner.nextDouble();
+	
+	System.out.println("Enter your weight:");
+	double weight = scanner.nextDouble();
+	
+    BMI b = new BMI(height, weight);
+
+System.out.println("BMI = " + b.calculateBMI());
+         }
+   }
+
 
